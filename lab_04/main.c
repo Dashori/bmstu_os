7bcd8573e610d7deb9b02914bb2164d575f59102
dashori <daahaaa@icloud.com>
2022-11-15 13:18:16 +0300

    pass

--- lab_04/main.c
+++ lab_04/main.c
@@ -1,4 +1,3 @@
-
 #include <string.h>
 #include <stddef.h>
 #include <pthread.h>
@@ -21,24 +20,68 @@
 void daemonize(const char *name);
 int lockfile(int fd);
 int already_running(void);
+void *my_thread(void *arg);
+
+sigset_t mask;
 
 int main(int argc, char *argv[])
 {
-    int error;
-    pthread_t thread_id;
-    char *cmd;
-    struct sigaction sa;
-
-    printf(" BBB ");
-
-    daemonize("dasha_deamon");
-
-    printf(" AAA ");
+    daemonize("dasha_daemon"); // меня за это побили
 
     if (already_running())
     {
-        syslog(LOG_ERR, "can\'t launch daemon - already running\n");
+        syslog(LOG_ERR, "Демон уже запущен\n");
         exit(1);
+    }
+    else
+    {
+        syslog(LOG_INFO, "Демон запущен\n");
+    }
+
+    int err;
+	pthread_t tid;
+    struct sigaction sa;
+
+    //Восстановить действие по умолчанию для сигнала SIGHUP
+    //и заблокировать все сигналы
+    
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGHUP, &sa, NULL) < 0)
+    {
+        syslog(LOG_ERR, "Невозможно восстановить действие SIG_DFL для SIGHUP\n");
+    }
+
+    sigfillset(&mask);
+    if ((err = pthread_sigmask(SIG_BLOCK, &mask, NULL)) != 0)
+    {
+        syslog(LOG_ERR, "Ошибка выполнения операции SIG_BLOCK\n");
+    }
+    
+    //Создание потока, который будет заниматься обработкой SIGHUP и SIGTERM
+     
+    err = pthread_create(&tid, NULL, my_thread, 0);
+	if (err != 0)
+    {
+        syslog(LOG_ERR, "Невозможно создать поток\n");
+    }
+    else
+    {
+        syslog(LOG_INFO, "Создан новый поток\n");
+    }
+
+    time_t timestamp;
+    struct tm *time_info;
+    syslog(LOG_DAEMON, "Юзер: %s", getlogin());
+    
+    for (;;)
+    {
+        sleep(SLEEP_TIME);
+        time(&timestamp);
+        time_info = localtime(&timestamp);
+        syslog(LOG_DAEMON, "%s", asctime(time_info));  
     }
 
     return 0;
@@ -56,19 +99,32 @@ void daemonize(const char *name)
 
     if (getrlimit(RLIMIT_NOFILE, &r_limit) < 0) 
     { 
-        syslog(LOG_ERR, "can\'t get file limit\n");
+        syslog(LOG_ERR, "Невозможно определить максимальный номер дескриптора файла\n");
     }
 
     pid_t deamon_pid;
 
-    if ((deamon_pid = fork()) < 0)
+    if ((deamon_pid = fork()) == -1)
     {
-        syslog(LOG_ERR, "can\'t fork\n");
+        syslog(LOG_ERR, "Ошибка вызова fork\n");
     } 
     else if (deamon_pid != 0)
     {
         exit(0);
     }
+
+    struct sigaction sa;
+
+    // обеспечиваем невозможность обретения управляющего терминала
+
+    sa.sa_handler = SIG_IGN; // макрос ignore this signal
+    sigemptyset(&sa.sa_mask); // установка empty mask
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGHUP, &sa, NULL) < 0) // SIGHUB сообщает процессу, что процесс утратил управляющий терминал
+    {
+        syslog(LOG_ERR, "Невозможно игнорировать SIGHUP\n");
+    }  
 
     // потомок теперь не является лидером группы и это является условием вызова функции setsid()
     // создаем сессию. При этом процесс становися лидером новой сессии, лидером новый группы процессов
@@ -76,29 +132,16 @@ void daemonize(const char *name)
 
     if (setsid() == -1) 
     {
-        syslog(LOG_ERR, "can\'t setsid\n");
+        syslog(LOG_ERR, "Ошибка вызова setsid\n");
         exit(1);
     }
-
-    struct sigaction sig_action;
-
-    // обеспечиваем невозможность обретения управляющего терминала
-
-    sig_action.sa_handler = SIG_IGN; // макрос ignore this signal
-    sigemptyset(&sig_action.sa_mask); // установка empty mask
-    sig_action.sa_flags = 0;
-
-    if (sigaction(SIGHUP, &sig_action, NULL) < 0) // SIGHUB сообщает процессу, что процесс утратил управляющий терминал
-    {
-        syslog(LOG_ERR, "can\'t ignore SIGHUP\n");
-    }  
 
     // меняем текущий каталог на корневой
     
 
     if (chdir("/") < 0) 
     {
-        syslog(LOG_ERR, "can\'t set directory to /\n");
+        syslog(LOG_ERR, "Невозможно изменить каталог на /\n");
     }
 
     // закрываем все открытые файловые дескрипторы
@@ -115,12 +158,11 @@ void daemonize(const char *name)
 
     // присоединяем файловые дескрипторы к 0, 1, 2 к /dev/null
 
-    int fd0, fd1, fd2;
+    int fd0, fd1, fd2; // stdin, stdout, stderr
 
-    fd0 = open("/dev/null", O_RDWR);
+    fd0 = open("/dev/null", O_RDWR); 
     fd1 = dup(0);
     fd2 = dup(0);
-
    
     // инициализируем файл журнала
 
@@ -128,7 +170,7 @@ void daemonize(const char *name)
 
     if (fd0 != 0 || fd1 != 1 || fd2 != 2)
     {
-        syslog(LOG_ERR, "unexpected file descriptors %d, %d, %d\n", fd0, fd1, fd2);
+        syslog(LOG_ERR, "Ошибка %d, %d, %d\n", fd0, fd1, fd2);
         exit(1);
     }
 
@@ -136,6 +178,8 @@ void daemonize(const char *name)
 
 int lockfile(int fd)
 {
+    // заполняем поля структуры
+    // настраиваем конфигирацию блокировки и применяем к дескриптору
     struct flock fl;
 
     fl.l_type = F_WRLCK;
@@ -155,7 +199,7 @@ int already_running(void)
 
     if (fd < 0)
     {
-        syslog(LOG_ERR, "can\'t open %s : %s\n", LOCKFILE, strerror(errno));
+        syslog(LOG_ERR, "Невозможно открыть %s : %s\n", LOCKFILE, strerror(errno));
         exit(1);
     }
 
@@ -165,11 +209,12 @@ int already_running(void)
         {
             close(fd);
 
-            return 1;
+            return 1; // если пытаемся создать копию
         }
 
-        syslog(LOG_ERR, "can't lock %s: %s\n", LOCKFILE, strerror(errno));
-        exit(1);
+        syslog(LOG_ERR, "Ошибка lock %s: %s\n", LOCKFILE, strerror(errno));
+
+        exit(1); // если ошибки блокировки 
     }
 
     // усечение размера файла необходимо по той причине, что 
@@ -180,4 +225,53 @@ int already_running(void)
     write(fd, buf, strlen(buf) + 1);
 
     return 0;
+}
+
+void reread(void) // перечитывает конфигурационный файл
+{
+    FILE *fd;
+    int pid;
+    
+    syslog(LOG_DAEMON, "Юзер: %s", getlogin());
+    
+    if ((fd = fopen("/var/run/daemon.pid", "r")) == NULL)
+    {
+        syslog(LOG_ERR, "Ошибка открытия файла /var/run/daemon.pid.");
+    }
+
+    fscanf(fd, "%d", &pid);
+    fclose(fd);
+
+    syslog(LOG_INFO, "Pid: %d", pid);
+}
+
+void *my_thread(void *arg)
+{
+    int err, signo;
+    for (;;)
+    {
+        err = sigwait(&mask, &signo);
+
+        if (err != 0)
+        {
+            syslog(LOG_ERR, "Ошибка вызова sigwait\n");
+            exit(1);
+        }
+
+        switch (signo)
+        {
+        case SIGHUP:
+            syslog(LOG_INFO, "Получен сигнал SIGHUB\n");
+            reread();
+            break;
+        case SIGTERM:
+            syslog(LOG_INFO, "Получен сигнал SIGTERM, выход\n");
+            exit(0);
+        default:
+            syslog(LOG_INFO, "Получен непредвиденный сигнал %d\n", signo);
+            break;
+        }
+    }
+
+    return (void*)0;
 }
